ec33ffa9d9b4c6a1ca85fe57f66ef0b5689e1940
sydney2312 <sydneyramnarace9@icloud.com>
2025-10-02 17:38:20 -0700

    Config projects store page

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,13 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use Illuminate\Support\Facades\Auth;
+
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        return true;
+        // check if the use is authenticated and which group the user belongs to
+        $group_ids = Auth::check() ? Auth::user()->getGroups() : [1];
+
+        // fetch data from products table
+        $product_data = Product::withPrices()->get();
+
+        // pass product information to views
+        return view('pages.default.productspage', compact('product_data'));
     }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,19 +4,19 @@ namespace App\Models;
 
 use App\Helpers\ProductCollectionHelper;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
     use HasFactory;
 
-
     /**
      * Create a new Eloquent Collection instance.
      *
-     * @param  array<int, \Illuminate\Database\Eloquent\Model>  $models
+     * @param array<int, \Illuminate\Database\Eloquent\Model> $models
+     *
      * @return \Illuminate\Database\Eloquent\Collection<int, \Illuminate\Database\Eloquent\Model>
      */
     public function newCollection(array $models = []): Collection
@@ -24,22 +24,38 @@ class Product extends Model
         return new ProductCollectionHelper($models);
     }
 
-    /**
-     *  =============== RELATIONSHIPS  ===============
-     */
+    public function scopeWithPrices(Builder $query, array $group_ids = [1])
+    {
+        $query->where('products.id', '>', 0);
+    }
 
+    public function scopeSingleProduct(Builder $query, int $id)
+    {
+        $query->where('products.id', $id);
+    }
 
-    /**
-     *  =============== SCOPES  ===============
-     */
+    public function getImage()
+    {
+        return asset('storage'.$this->image_path.$this->image_name);
+    }
 
+    public function getPrice()
+    {
+        return $this->price;
+    }
 
-    /**
-     *  =============== FUNCTIONS  ===============
-     */
+    public function getStockPrice()
+    {
+        return $this->price;
+    }
 
+    public function getCartQuantityPrice()
+    {
+        return $this->getPrice() * $this->pivot->quantity;
+    }
 
-
-
-
+    public function getLink()
+    {
+        return route('shop.details', ['id' => $this->id]);
+    }
 }
